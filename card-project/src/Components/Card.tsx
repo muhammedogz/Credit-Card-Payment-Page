3651be3b2ef85cdd0f4daa55e1e610e12bd2c4b0
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2022-01-23 23:25:12 +0300

    Add card number input

--- card-project/src/Components/Card.tsx
+++ card-project/src/Components/Card.tsx
@@ -6,24 +6,42 @@ import './Card.scss';
 
 const front = '/card_front.jpg';
 const back = '/card_back.jpg';
-const mastercard = '/mastercard.png';
-const visa = '/visa.png';
 
-console.log(process.env.PUBLIC_URL)
+interface CardProps {
+    cardType: string;
+    cardNumber: string;
+    cardDate: string;
+    cardCCV: string;
+}
 
-const Card = () => {
+const Card = ( { cardType, cardNumber, cardCCV, cardDate } : CardProps ) => {
 
     const [isFlipped, setIsFlipped] = useState(false);
     const [showText, setShowText] = useState(true);
-    const [isMasterCard, setIsMasterCard] = useState(true);
-    const [isVisa, setIsVisa] = useState(false);
-
-    const [cardType, setCardType] = useState(mastercard);
 
 
+
+    let leftSide = '';
+    let rightSide = '';
+
+    const splitStringToLeftRight = (string: string) => {
+        const splitString = string.split('');
+
+        const length = splitString.length;
+        // get first 4 digits
+        leftSide = splitString.slice(0, 4).join('');
+        if (length > 12){
+            const showValue = length - 12;
+            rightSide = splitString.slice(length - showValue).join('');
+        }
+        // get last 4 digits
+    }
+
+    splitStringToLeftRight(cardNumber);
 
     const handleClick = () => {
         setIsFlipped(!isFlipped);
+        
         // wait .1 seconds before flipping back
         setTimeout(() => {
             setShowText(!showText);
@@ -43,12 +61,15 @@ const Card = () => {
                     <img src={process.env.PUBLIC_URL + back} alt="back" />
                 </div>
             </ReactCardFlip>
-            {(!isFlipped && showText) ? <><p className="card-text card-text-left"> 0545 </p>
-            <p className="card-text card-text-right"> 0545 </p>
-            <p className="card-text card-time">Date: 04/23</p>
-            <img className="card-brand" src={process.env.PUBLIC_URL + cardType } alt="mastercard" /></> : 
+            {(!isFlipped && showText) ? 
             <>
-                <p className="card-text card-ccv" >CCV: 321</p>
+                <p className="card-text card-text-left"> {leftSide} </p>
+                <p className="card-text card-text-right"> {rightSide} </p>
+                <p className="card-text card-time">Date: {cardDate}</p>
+                <img className="card-brand" src={process.env.PUBLIC_URL + cardType } alt={cardType} />
+            </> : 
+            <>
+                <p className="card-text card-ccv">CCV: {cardCCV} </p>
             </>
             }
         </div>
--- card-project/src/Components/CardLayout.tsx
+++ card-project/src/Components/CardLayout.tsx
@@ -1,10 +1,49 @@
+import { useState } from 'react';
+
 import Card from './Card'
 import './CardLayout.scss';
 
+const mastercard = '/mastercard.png';
+const visa = '/visa.png';
+
 const CardLayout = () => {
+
+    const [isMasterCard, setIsMasterCard] = useState(false);
+    const [cardType, setCardType] = useState('');
+    const [cardNumber, setCardNumber] = useState('');
+    const [cardCCV, setCardCCV] = useState('');
+    const [cardDate, setCardDate] = useState('');
+
+    const checkCardNumber = (cardNumber: string) => {
+        const cardNumberLength = cardNumber.length;
+        // if card contains alphabets
+        // delete last character
+
+        setCardNumber(cardNumber.replace(/[^0-9]/g, ''));
+            
+    }
+
+
     return (
         <div className='center-div'>
-            <Card />
+            <Card cardType={cardType} cardNumber={cardNumber} cardDate={cardDate} cardCCV={cardCCV} />
+            <div className='form-div'>
+                <div className='input-form'>
+                    <label >Card Number
+                    <input 
+                        type='text' 
+                        name='card-num'
+                        id='card-num'
+                        placeholder='Enter Card Number'
+                        max={16}
+                        pattern="[0-9]{16}"
+                        maxLength={16}
+                        value={cardNumber}
+                        onChange={(e) => checkCardNumber(e.target.value)}
+                    />
+                    </label>
+                </div>
+            </div>
         </div>
     )
 }

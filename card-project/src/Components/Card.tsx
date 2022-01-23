a4e7ccc36483768e6119c44fafad44db2d81cd1e
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2022-01-23 21:46:27 +0300

    Add text to card

--- card-project/src/Components/Card.tsx
+++ card-project/src/Components/Card.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 import ReactCardFlip from 'react-card-flip';
 
+import './Card.scss';
+
 const front = '/card_front.jpg';
 const back = '/card_back.jpg';
 
@@ -14,16 +16,18 @@ const Card = () => {
 
     return (
         <>
-        <div onClick={() => setIsFlipped(!isFlipped)}>
-        <ReactCardFlip isFlipped={isFlipped}>
-            <div key="front">
-                <img src={process.env.PUBLIC_URL + front} alt="front" />
-            </div>
+        <div className="card-div" onClick={() => setIsFlipped(!isFlipped)}>
+            <ReactCardFlip isFlipped={isFlipped}>
+                <div key="front">
+                    <img src={process.env.PUBLIC_URL + front} alt="front" />
+                </div>
 
-            <div key="back">
-                <img src={process.env.PUBLIC_URL + back} alt="back" />
-            </div>
-        </ReactCardFlip>
+                <div key="back">
+                    <img src={process.env.PUBLIC_URL + back} alt="back" />
+                </div>
+            </ReactCardFlip>
+            {!isFlipped && <><p style={{backgroundColor:"re"}} className="card-text card-text-left"> 0545 </p>
+            <p className="card-text card-text-right"> 0545 </p></>}
         </div>
         </>
     )

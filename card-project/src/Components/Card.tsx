84b89c506ac6c98fb1e2ca92d86972e7bdbbeb7c
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2022-01-23 21:57:21 +0300

    Add delay when showing number

--- card-project/src/Components/Card.tsx
+++ card-project/src/Components/Card.tsx
@@ -12,11 +12,20 @@ console.log(process.env.PUBLIC_URL)
 const Card = () => {
 
     const [isFlipped, setIsFlipped] = useState(false);
+    const [showText, setShowText] = useState(true);
 
+    const handleClick = () => {
+        setIsFlipped(!isFlipped);
+        // wait .1 seconds before flipping back
+        setTimeout(() => {
+            setShowText(!showText);
+        } , 300);
+
+    }
 
     return (
         <>
-        <div className="card-div" onClick={() => setIsFlipped(!isFlipped)}>
+        <div className="card-div" onClick={handleClick}>
             <ReactCardFlip isFlipped={isFlipped}>
                 <div key="front">
                     <img src={process.env.PUBLIC_URL + front} alt="front" />
@@ -26,7 +35,7 @@ const Card = () => {
                     <img src={process.env.PUBLIC_URL + back} alt="back" />
                 </div>
             </ReactCardFlip>
-            {!isFlipped && <><p style={{backgroundColor:"re"}} className="card-text card-text-left"> 0545 </p>
+            {!isFlipped && showText && <><p className="card-text card-text-left"> 0545 </p>
             <p className="card-text card-text-right"> 0545 </p></>}
         </div>
         </>
